1b5c831157d8f86312581bb4620d274d9a3074ab
androustsang <androustsang@gmail.com>
2024-09-29 23:57:32 -0400

    For Exercise 2

--- Assignment/src/lab2/Lotto.java
+++ Assignment/src/lab2/Lotto.java
@@ -8,8 +8,12 @@ public class Lotto {
 
     // create secure random number generator for use in method
     private static final SecureRandom randomNumbers = new SecureRandom();
+
     // enum type with constants that represent the game status
-    private enum Status {CONTINUE, WON, LOST};
+    //WON represent User Win, Lost represent Computer Win
+    private enum Status {CONTINUE, USERWON, USERLOST};
+
+    // internal integer array to store the lotto number
     private int[] lottoArray;
 
         // Constructor methods
@@ -17,12 +21,12 @@ public class Lotto {
         this.lottoArray = new int[3]; // initialize the array of 3 integers for a lotto object
 
         for (int i = 0; i < 3; i++) { // populate the values into the array
-            this.lottoArray[i] = 1 + randomNumbers.nextInt(9);
+            this.lottoArray[i] = 1 + randomNumbers.nextInt(9); // Each number is bound by 9
         }
     }
 
     public int[] getLottoArray(){
-            return this.lottoArray;
+            return this.lottoArray; // To return the integer array
     }
 
     // Run the game
@@ -30,15 +34,18 @@ public class Lotto {
         Object[] userOptions = {3,4,5,6,7,8,9,10,
                 11,12,13,14,15,16,17,18,19,20,
                 21,22,23,24,25,26,27}; //Allowed choices for user
-
+        Status gameStatus; // Can continue CONTINUE, USERWON, USERLOST
 
         for (int i = 0; i < 5; i++) {   // Runs the lotto up to 5 times
             Lotto gameLotto = new Lotto();  // Get a new lotto each time
+            int[] gameLottoArray = gameLotto.getLottoArray();
+            int gameLottoSum = 0;
 
-            int[] gameLottoArray = gamelotto.getLottoArray();
-            for (int j = 0; j < lottosum.length; j++) {
-
+            for (int j = 0; j < gameLottoArray.length; j++) {
+                gameLottoSum += gameLottoArray[j];
             }
+
+            System.out.println("The Lotto Sum is: " + gameLottoSum);
 
             Object userSelection = JOptionPane.showInputDialog(null,
                     "Please choose between 3 and 27",
@@ -47,14 +54,21 @@ public class Lotto {
                     null,
                     userOptions,
                     userOptions[0]);
-            if (userSelection != null) {
+            if (userSelection != null) { // Check if the user click the Cancel button
+                    if ((int) userSelection == gameLottoSum) {
+                        gameStatus = Status.USERWON;
+                        JOptionPane.showMessageDialog(null, "Congratulation! You win the game!");
+                        break;
+                    } else {
+                        gameStatus = Status.CONTINUE;
+                    }
                 System.out.println("User selection " + (int) userSelection);
-
             }
+        } // End of for-loop
+        gameStatus = Status.USERLOST;
 
-
-        }
     }
+
 
 
 }
