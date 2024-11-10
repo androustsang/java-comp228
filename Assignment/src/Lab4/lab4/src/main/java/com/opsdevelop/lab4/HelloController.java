8bd5e0ef6dc8b37b186eeb728b63703c0ea68abb
androustsang <androustsang@gmail.com>
2024-11-10 00:58:37 -0500

    Lab 4 with comments
    Update some styles and comments

--- Assignment/src/Lab4/lab4/src/main/java/com/opsdevelop/lab4/HelloController.java
+++ Assignment/src/Lab4/lab4/src/main/java/com/opsdevelop/lab4/HelloController.java
@@ -4,9 +4,9 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
 
 public class HelloController {
+    // Define the list of courses for each program / major
     private String[] coursesComputerScience = {"Java", "Python", "C#", "JavaScript"};
     private String[] coursesBusiness = {"Accounting", "Marketing", "Human Resources","Organizational Behaviour"};
 
@@ -30,44 +30,47 @@ public class HelloController {
 
     @FXML
     private void initialize() {
-//        nameLabel.setPrefHeight(nameTextField.getHeight());
-//        addressLabel.prefHeightProperty().bind(addressTextField.heightProperty());
+        // if there is any initialization to be done, it is ran automatically in this method.
     }
 
+    // Method to handle when either of the program / major radio button is clicked
     @FXML
     protected void progRadioButtonChanged(ActionEvent event) {
-        if (progCsRadio.isSelected()) {
-            courseComboBox.getItems().setAll(coursesComputerScience);
+        if (progCsRadio.isSelected()) {  // if Computer Science button is selected
+            courseComboBox.getItems().setAll(coursesComputerScience); // update the ComboBox to the corresponding list
             courseListView.getItems().clear();  // Clear the current items if the Program is changed
-        } else if (progBizRadio.isSelected()) {
-            courseComboBox.getItems().setAll(coursesBusiness);
+        } else if (progBizRadio.isSelected()) {  //if Business button is selected
+            courseComboBox.getItems().setAll(coursesBusiness); //update the ComboBox to the corresponding list
             courseListView.getItems().clear();  // Clear the current items if the Program is changed
         } else {
             courseComboBox.getItems().clear();
         }
     }
 
+    // Method to handle when user select from ComboBox and add to the ListView
     @FXML
     protected void courseComboBoxChanged(ActionEvent event) {
         String newSelectedCourse = courseComboBox.getSelectionModel().getSelectedItem();
 //        System.out.println(courseComboBox.getSelectionModel().getSelectedItem());
 
 //        String[] currentSelectedCourses = courseListView.getSelectionModel().getSelectedItem().split(",");
-//        String[] currentSelectedCourses = courseListView.getSelectionModel().getSelectedItems().toArray(new String[0]);
-        ObservableList<String> currentSelectedCourses = courseListView.getItems();
+        ObservableList<String> currentSelectedCourses = courseListView.getItems(); // Get the current list in ListView
 
-        System.out.println("Before changed: " + currentSelectedCourses);
+//        System.out.println("Before changed: " + currentSelectedCourses); //For debugging
 
+        // if the selected course is already in the ListView , print for debugging
         if (newSelectedCourse == null || currentSelectedCourses.contains(newSelectedCourse)) {
-            System.out.println("Course selected is empty or already in the list:" + newSelectedCourse);
+            System.out.println("Course selected is empty or already in the list:" + newSelectedCourse); //For debugging
         } else {
-            System.out.println("Course selected as new course:" + newSelectedCourse);
+            //Only add the course if it is not in the current ListView
+            System.out.println("Course selected as new course:" + newSelectedCourse); //For debugging
             courseListView.getItems().add(newSelectedCourse);
         }
 
-        System.out.println("After changed: " + currentSelectedCourses);
+        System.out.println("After changed: " + currentSelectedCourses); //For debugging
     }
 
+    // Method to handle when the Display button is clicked
     @FXML
     protected void displayBtnClicked(ActionEvent event) {
         // using trim() method to remove whitespaces at both ends
@@ -78,32 +81,42 @@ public class HelloController {
         String postal = postalTextField.getText().trim();
         String phone = phoneTextField.getText().trim();
         String email = emailTextField.getText().trim();
+        // Get all the textfield values
+
+        // Get all the selected courses
         ObservableList<String> courses = courseListView.getItems();
+
+        // Variables to be used for storing the program and other activities
         String selectedProgram = "";
         String activities = "";
 
+        // Store the CheckBox text if it is selected
         String activityStudCouncil = studCouncilCheckBox.isSelected() ? studCouncilCheckBox.getText() : "";
         String activityVolunteer = volunteerWorkCheckBox.isSelected() ? volunteerWorkCheckBox.getText() : "";
 
-        // Check if any text input fields are empty, warn the user and quit
+        // Validation check if any text input fields are empty, warn the user and quit
         if (name.isBlank() || address.isBlank() || province.isBlank() || city.isBlank()|| postal.isBlank() || phone.isBlank() || email.isBlank()) {
             System.out.println("Some text fields are empty");
             showErrorAlert("All text fields must be filled");
             return;
-        // Check if the courses list is empty, warn the user and quit
+        // Validation check if the courses list is empty, warn the user and quit
         } else if (courses.isEmpty()) {
             System.out.println("Courses List View is empty");
             showErrorAlert("Please select at least one course");
             return;
         }
 
+        // Get all the Student Information from the text field and store in string
         String studentInfo = String.join(", ", name, address, province, city, postal, phone, email);
+
+        // Store in string from the above courses from ListView
         String selectedCourses = String.join("\n", courses);
 
-        // Get the selected toggle directly to get the Program chosen from the Radio button
-        Toggle selectedToggle = programToggle.getSelectedToggle();
+        // Get the selected toggle group directly to get the Program chosen from the Radio button
+        Toggle selectedToggle = programToggle.getSelectedToggle(); // This gets the selected UI control (as toggle)
         if (selectedToggle != null) {
             RadioButton selectedRadioButton = (RadioButton) selectedToggle ;
+            //As Toggle is parent class with less methods, Cast to RadioButton to use other methods
             selectedProgram = selectedRadioButton.getText();
             System.out.println(selectedProgram);
         } else {
@@ -117,9 +130,9 @@ public class HelloController {
             activities = !activityVolunteer.isEmpty() ? String.join("\n", activities,activityVolunteer) : activities;
         }
 
-        System.out.println(studentInfo);
-        System.out.println(selectedCourses);
-        System.out.println(activities);
+        System.out.println(studentInfo); //For debugging
+        System.out.println(selectedCourses); //For debugging
+        System.out.println(activities); //For debugging
 
 
         // Update the text area for all the information about the student to be displayed
@@ -130,6 +143,7 @@ public class HelloController {
 
     }
 
+    // Method to show error alert that if certain fields are empty
     private void showErrorAlert(String message) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
@@ -138,6 +152,7 @@ public class HelloController {
         alert.showAndWait();
     }
 
+    // Method to clear all the input fields
     private void clearInputFields() {
         nameTextField.clear();
         addressTextField.clear();
