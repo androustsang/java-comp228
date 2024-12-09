8fff13573aa776204e3976382288be311fe149d9
androustsang <androustsang@gmail.com>
2024-12-09 13:01:44 -0500

    Lab 6 updates

--- Assignment/src/Lab6/src/AccountTest.java
+++ Assignment/src/Lab6/src/AccountTest.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class AccountTest {
     public static void main(String[] args) {
         // Create a thread pool of 2
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
+//        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
+        ExecutorService executor = Executors.newFixedThreadPool(2);
 
         // Create an ArrayList to hold all transaction objects
         int numOfTransactions = 3;
